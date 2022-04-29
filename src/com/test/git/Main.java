4079d23895f6bdc7ba5c7b1d8be92fd657c4c142
venkatesh <venkaetsh.srikondaone@gmail.com>
2022-04-29 06:54:28 +0530

    to test release

--- src/com/test/git/Main.java
+++ src/com/test/git/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hrllo.....");
+System.out.println("from release test commit.....");
     }
 }
